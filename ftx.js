b8fb5871a571884f2b1021250e0b2107792035ca
wancat <wancat@wancat.cc>
2021-10-12 21:40:17 +0800

    get balances function

--- ftx.js
+++ ftx.js
@@ -13,23 +13,24 @@ const hmac = createHmac("sha256", config.api_secret)
 api.interceptors.request.use(function (req) {
   const ts = Date.now()
   const body = `${ts}${req.method.toUpperCase()}${req.url}${req.body? req.body : ""}`
-  console.log(body)
   hmac.update(body)
   req.headers["FTX-KEY"] = config.api_key
   req.headers["FTX-TS"] = String(ts)
   req.headers["FTX-SIGN"] = hmac.digest("hex")
-  console.log(req)
   return req
 }, function (err) {
   return Promise.reject(err)
 })
 
-async function main() {
-  const res = await api.get("/api/account")
-  console.dir(res.data)
+async function balances() {
+  const res = await api.get("/api/wallet/balances")
+  return res.data.result
 }
 
-main()
+balances()
+  .then((result) => {
+    console.log(result)
+  })
   .catch((err) => {
     console.log(err.toJSON())
   })
